4cc971f995f91d2a7567d25544001cea7eac6975
elsa <elsa@gmail.com>
2024-11-17 19:37:18 +0100

    RegisterNewCar Page Updated

--- src/main/java/full/stack/parkspring/frontend/registerNewCarController.java
+++ src/main/java/full/stack/parkspring/frontend/registerNewCarController.java
@@ -1,4 +1,125 @@
 package full.stack.parkspring.frontend;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+
 public class registerNewCarController {
+
+    @FXML
+    private TextField CarModelField;
+
+    @FXML
+    private TextField ClassField;
+
+    @FXML
+    private TextField ColorField;
+
+    @FXML
+    private TextField LicenseNumberField;
+
+    @FXML
+    private TextField LicensePlateField;
+
+    @FXML
+    private TextField NameField;
+
+    @FXML
+    private Button RegisterButton;
+
+    @FXML
+    private VBox avatarMenu;
+
+    @FXML
+    private Label homeButton;
+
+    @FXML
+    private Label homeButton1;
+
+    @FXML
+    private Line mBar1;
+
+    @FXML
+    private Line mBar2;
+
+    @FXML
+    private Line mBar3;
+
+    @FXML
+    private Label menuBars;
+
+    @FXML
+    private ImageView profileAvatarIcon;
+
+    @FXML
+    private Label reserveLabel;
+
+    @FXML
+    private TextField searchBarField;
+
+    @FXML
+    private Label signInButton;
+
+    @FXML
+    void SignInButtonOnClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void homeButtonOnClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onLabelExitHoverHome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onLabelExitHoverReserve(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onLabelHoverHome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onLabelHoverReserve(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onPolylineExit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onPolylineHover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reserveButtonOnClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void setInvalidRegisterMessageOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toggleMenu(MouseEvent event) {
+
+    }
+
 }
+
